b03ab86d6ca4e379bc5c9f7256cd02575722b20a
Maetschl <maetschl@gmail.com>
2023-11-10 17:02:54 -0300

    rollback

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
 	name: "DiscordKit",
 	platforms: [
-		.macOS(.v10_13)
+		.macOS(.v11)
     ],
 	products: [
         .library(name: "DiscordKitCore", targets: ["DiscordKitCore"]),
